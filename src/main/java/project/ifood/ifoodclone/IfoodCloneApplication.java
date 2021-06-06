a0d28b37d5e73ace331e0d6f0a9d4bcb67e18a00
samuelalmeida95 <samuelalmeida95>
2021-06-06 14:19:06 -0300

    iniciando projeto IfoodClone

---
+++ src/main/java/project/ifood/ifoodclone/IfoodCloneApplication.java
@@ -0,0 +1,13 @@
+package project.ifood.ifoodclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfoodCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfoodCloneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/ifood/ifoodclone/IfoodCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package project.ifood.ifoodclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfoodCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
