5bc2b87d264fda1441fef2b94b7b6e00bee35ee4
samuelalmeida95 <samuelalmeida95>
2021-06-06 14:37:55 -0300

    adicionando entidade User e Controller para User

---
+++ src/main/java/project/ifood/ifoodclone/model/User.java
@@ -0,0 +1,86 @@
+package project.ifood.ifoodclone.model;
+
+import java.io.Serializable;
+
+public class User implements Serializable{
+    private static final long serialVersionUID = 1L;
+    private Long id;
+    private String email;
+    private String nome;
+    private String phone;
+    private String password;
+
+    public User(Long id, String email, String nome, String phone, String password) {
+        this.id = id;
+        this.email = email;
+        this.nome = nome;
+        this.phone = phone;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+    
+}
