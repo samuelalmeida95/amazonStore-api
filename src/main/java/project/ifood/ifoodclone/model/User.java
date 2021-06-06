b7988d4ff78d00188ce48a91935d0f8111494d63
samuelalmeida95 <samuelalmeida95>
2021-06-06 14:58:16 -0300

    configurando H2, JPA e perfil de teste

--- src/main/java/project/ifood/ifoodclone/model/User.java
+++ src/main/java/project/ifood/ifoodclone/model/User.java
@@ -2,9 +2,19 @@ package project.ifood.ifoodclone.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable{
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String email;
     private String nome;
     private String phone;
