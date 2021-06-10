a9cbf0195439d4b6a5be0306a351bc5f0f069251
samuelalmeida95 <samuelalmeida95>
2021-06-10 13:44:08 -0300

    OrderItem, many-to-many association with extra attributes PART 2

--- src/main/java/project/ifood/ifoodclone/configs/TestConfig.java
+++ src/main/java/project/ifood/ifoodclone/configs/TestConfig.java
@@ -11,9 +11,11 @@ import org.springframework.context.annotation.Profile;
 import project.ifood.ifoodclone.enums.OrderStatus;
 import project.ifood.ifoodclone.models.Category;
 import project.ifood.ifoodclone.models.Order;
+import project.ifood.ifoodclone.models.OrderItem;
 import project.ifood.ifoodclone.models.Product;
 import project.ifood.ifoodclone.models.User;
 import project.ifood.ifoodclone.repository.CategoryRepository;
+import project.ifood.ifoodclone.repository.OrderItemRepository;
 import project.ifood.ifoodclone.repository.OrderRepository;
 import project.ifood.ifoodclone.repository.ProductRepository;
 import project.ifood.ifoodclone.repository.UserRepository;
@@ -34,6 +36,9 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private ProductRepository productRepository;
 
+    @Autowired
+    private OrderItemRepository orderItemRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
@@ -46,14 +51,14 @@ public class TestConfig implements CommandLineRunner {
         Category cat1 = new Category(null, "Electronics");
         Category cat2 = new Category(null, "Books");
         Category cat3 = new Category(null, "Computers");
-  
+
         User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
         User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
-        
+
         Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u2);
         Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.CANCELED, u2);
         Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.SHIPPED, u1);
-        
+
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
         userRepository.saveAll(Arrays.asList(u1, u2));
@@ -66,7 +71,13 @@ public class TestConfig implements CommandLineRunner {
         p4.getCategories().add(cat3);
         p5.getCategories().add(cat2);
 
-        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4,p5));
-    }
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+    }
 }
--- src/main/java/project/ifood/ifoodclone/models/Order.java
+++ src/main/java/project/ifood/ifoodclone/models/Order.java
@@ -2,6 +2,8 @@ package project.ifood.ifoodclone.models;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,6 +11,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -32,6 +35,9 @@ public class Order implements Serializable {
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
+
+    @OneToMany(mappedBy = "id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
@@ -74,6 +80,10 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public Set<OrderItem> getItems() {
+        return items;
     }
 
     @Override
